83e2794967bc0cd088c8165ddddbfb6a01732811
gleiton.franco <gleiton.franco@mj.gov.br>
2022-02-07 18:26:24 -0300

    * 2.04: FormsModule: ngSubmit; * 2.05: Data binding: - property binding; - event binding; - 2 way data binding;

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss']
 })
 export class NovaTransferenciaComponent implements OnInit {
+  valor: number = 22;
+  destino: number = 2222;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  transferir() {
+    console.log('transferido!', this.valor, this.destino);
   }
 
 }
